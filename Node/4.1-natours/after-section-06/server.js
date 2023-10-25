f4d4553965bd47c5d0cb2b4a0b91d7323b35d37a
002 <anbschool0002@002ui-Macmini.local>
2023-10-25 20:47:00 +0700

    lol

--- Node/4.1-natours/after-section-06/server.js
+++ Node/4.1-natours/after-section-06/server.js
@@ -1,7 +1,12 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const mongoose = require('mongoose')
 
 dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.DB)
+.then()
+.catch((err) => console.log(err))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
