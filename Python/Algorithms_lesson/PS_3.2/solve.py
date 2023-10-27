0a7e3d428263a43b1c101e0397fd88893fa4e8ad
004 <anbschool0004@004ui-Macmini.local>
2023-10-27 13:21:57 +0700

    updata python

--- Python/Algorithms_lesson/PS_3.2/solve.py
+++ Python/Algorithms_lesson/PS_3.2/solve.py
@@ -1,9 +1,12 @@
 def solve(n,p) :
     p.sort(key = lambda x : (x[0], -x[1]))
+    print("psorted :",p)
     for i in range(n) :
+        print(p[i])
         print(" ".join(map(str, p[i])))
 
 n = int(input())
 p = [list(map(int,input().split())) for _ in range (n)]
+print("p :",p)
 print("Sorted")
 solve(n,p)
--- Python/Algorithms_lesson/PS_3.2/sort_lamda.py
+++ Python/Algorithms_lesson/PS_3.2/sort_lamda.py
@@ -7,10 +7,16 @@ lst = [(2,5),(2,1),(1,3),(1,4),(1,2)]
 
 # passing tuple
 print("Sort by tuple :",end=" ")
-print(sorted(lst, key =  lambda k : (k[0],k[1])))
+print(sorted(lst, key =  lambda k : (k[0],k[1]))) # sort y asending when x are the same
 
 print("Sort by tuple :",end=" ")
-print(sorted(lst, key =  lambda k : (k[0],-k[1])))
+print(sorted(lst, key =  lambda k : (k[0],-k[1]))) # sort y desending when x are the same
 
 lst.sort(key = lambda k : (k[0],-k[1]))
 print("lst.sort() :",lst)
+
+#---------------------------------------------------------------------------------------
+# it sort by in asending order by x but when it a tie of x it will sort in asending by y
+lst = [(3,5),(2,1),(4,3),(1,4),(1,2)]
+print("Sort zzz :",end=" ")
+print(sorted(lst, key =  lambda k : (k[0],k[1]))) # sort y asending when x are the same
