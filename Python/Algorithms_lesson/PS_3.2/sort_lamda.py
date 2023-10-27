e9ea0da74227ac4beea70a0f9eb6ea0f7a30a468
004 <anbschool0004@004ui-Macmini.local>
2023-10-27 13:26:05 +0700

    Revert "updata python"
    This reverts commit 0a7e3d428263a43b1c101e0397fd88893fa4e8ad.

--- Python/Algorithms_lesson/PS_3.2/sort_lamda.py
+++ Python/Algorithms_lesson/PS_3.2/sort_lamda.py
@@ -7,16 +7,10 @@ lst = [(2,5),(2,1),(1,3),(1,4),(1,2)]
 
 # passing tuple
 print("Sort by tuple :",end=" ")
-print(sorted(lst, key =  lambda k : (k[0],k[1]))) # sort y asending when x are the same
+print(sorted(lst, key =  lambda k : (k[0],k[1])))
 
 print("Sort by tuple :",end=" ")
-print(sorted(lst, key =  lambda k : (k[0],-k[1]))) # sort y desending when x are the same
+print(sorted(lst, key =  lambda k : (k[0],-k[1])))
 
 lst.sort(key = lambda k : (k[0],-k[1]))
 print("lst.sort() :",lst)
-
-#---------------------------------------------------------------------------------------
-# it sort by in asending order by x but when it a tie of x it will sort in asending by y
-lst = [(3,5),(2,1),(4,3),(1,4),(1,2)]
-print("Sort zzz :",end=" ")
-print(sorted(lst, key =  lambda k : (k[0],k[1]))) # sort y asending when x are the same
